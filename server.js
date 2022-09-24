9f71eec31bc8ed4ead7d6957fddb9133ee73ad5d
Kousthub Shetty <kousthubshetty@gmail.com>
2022-09-24 15:05:21 +0530

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express= require("express");
 const app= express();
 const fs = require('fs')
+const path = require('path');
 const youtubedl = require('youtube-dl')
 const request = require('request')
 const port = process.env.PORT || 3000;
@@ -13,10 +14,11 @@ const download = (url, path, callback) => {
 
 app.get('/download',(req, res) => {
     const url = req.query['url']
-    const path = './myvideo.mp4'
-    download(url, path, () => {
+    const spath = './myvideo.mp4'
+    download(url, spath, () => {
         console.log('✅ Downloaded on server machine!')
-        filepath='/Users/kousthubshetty/Downloads/programs/NodeJS/download-videos/myvideo.mp4';
+        let relativePath = './myvideo.mp4';
+        filepath=path.resolve(relativePath);
         filename='ur_42'+Math.random()+'.mp4'
         res.download(filepath,filename,()=>{console.log('✅ Downloaded on client machine!')});
       })
